5c811f65c5adc4d701c9a1e960b7fb4aa3afed71
Arav <71561508+zCranking@users.noreply.github.com>
2022-01-09 09:59:33 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var paddle2Y = 685,paddle2Height = 70;
 var score1 = 0, score2 =0;
 var paddle1Y;
 
-var  playerscore =0;
+var  playerscore =5;
 var audio1;
 var pcscore =0;
 //ball x and y and speedx speed y and radius
