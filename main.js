035de6f671ea76058687b8695e87f5e6b88577d4
Arav <71561508+zCranking@users.noreply.github.com>
2022-01-10 13:03:18 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,14 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+	video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+function modelLoaded(){
+  console.log("model loaded");
 }
 
 
